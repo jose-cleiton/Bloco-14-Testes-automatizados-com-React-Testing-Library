4b028019a178212bd3d3d359eef5a8b93c27d3e7
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-03-21 20:04:29 -0300

    Os Mocks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./styles.css";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  numeroAleatorio = () => Math.floor(Math.random() * 100);
+  divisivelPorDois = () => this.numeroAleatorio() % 2 === 0;
+
+  render() {
+    console.log(this.divisivelPorDois());
+    const { numeroAleatorio, divisivelPorDois } = this;
+    return (
+      <div className="App">
+        <h1>{divisivelPorDois()}</h1>
+        <h2>{numeroAleatorio() ? <p> Sim</p> : <p>Não</p>}</h2>
+      </div>
+    );
+  }
 }
